e2d70e63d79ded698082c6fd474ff0984f822989
manta <mantas.zaliaduonis@gmail.com>
2025-11-06 15:17:59 +0200

    Finsihed game logic game end after one off the players dies

--- src/main/java/cupcakes/burtininko_dvikova/game_logic/GameLogic.java
+++ src/main/java/cupcakes/burtininko_dvikova/game_logic/GameLogic.java
@@ -3,8 +3,7 @@ package cupcakes.burtininko_dvikova.game_logic;
 import cupcakes.burtininko_dvikova.model.Spell;
 import cupcakes.burtininko_dvikova.model.Wizard;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class GameLogic {
@@ -18,29 +17,40 @@ public class GameLogic {
         System.out.println(player.getName() + " has " + player.getHealth() + " health and " + player.getMana() + " mana.");
         System.out.println(opponent.getName() + " has " + opponent.getHealth() + " health and " + opponent.getMana() + " mana.");
         System.out.println("All available spells:\n");
-        int i =0;
-        for(Spell spell : Spell.values())
-        {
-            i++;
-            System.out.println(spell.getNum()+ " "+ spell.getName() + " - Mana Cost: " + spell.getManaCost() + ", Damage: " + spell.getDamage() + ", Healing: " + spell.getHealing());
-        }
+
         while(isAlive(player) && isAlive(opponent)) {
+            System.out.println("Your stats: " + player.getHealth() + " health and " + player.getMana() + " mana.");
+            System.out.println("Your turn to choose a spell:");
+            for(Spell spell : Spell.values())
+            {
+                System.out.println(spell.getNum()+ " "+ spell.getName() + " - Mana Cost: " + spell.getManaCost() + ", Damage: " + spell.getDamage() + ", Healing: " + spell.getHealing());
+            }
 
             System.out.println("Choose your spell by its number:");
             int choice = scanner.nextInt();
             if (choice == 1) {
-                Spell spell = Spell.values()[0];
-                System.out.println("You cast " + spell.getName() + "!");
+                calculateDamage(Spell.values()[0], player, opponent);
             } else if (choice == 2) {
-                Spell spell = Spell.values()[1];
-                System.out.println("You cast " + spell.getName() + "!");
+                calculateDamage(Spell.values()[1], player, opponent);
             } else if (choice == 3) {
-                Spell spell = Spell.values()[2];
-                System.out.println("You cast " + spell.getName() + "!");
+                calculateDamage(Spell.values()[2], player, opponent);
             } else {
                 System.out.println("Invalid choice you lost 10 off your health.");
-//            player.setHealth(player.getHealth() - 10); veliau padryt
+//            player.setHealth(player.getHealth() - 10); veliau padryt ir jog ismestu vel pasirenkima
             }
+            System.out.println("Opponent's stats: " + opponent.getHealth() + " health and " + opponent.getMana() + " mana.");
+            if(isAlive(opponent)) {
+                System.out.println("Opponent is still alive.");
+            } else {
+                break;
+            }
+            System.out.println("Opponent is choosing a spell...");
+            int aiChoice = aiMove();
+            System.out.println("Opponent chose " + Spell.values()[aiChoice].getName());
+            calculateDamage(Spell.values()[aiChoice], opponent, player);
+
+
+
         }
         if (isAlive(player)) {
             System.out.println("Good job you won");
@@ -49,19 +59,22 @@ public class GameLogic {
         }
 
     }
-    public int calculateDamage(Spell spell, Wizard caster, Wizard target) {
+    public static void calculateDamage(Spell spell, Wizard caster, Wizard target) {
         if (caster.getMana() < spell.getManaCost()) {
             System.out.println(caster.getName() + " does not have enough mana to cast " + spell.getName() + "! you lost 5 off your health.");
 //            caster.setHealth(caster.getHealth() - 5); veliau padryt
-            return 0;
         }
         caster.setMana(caster.getMana() - spell.getManaCost());
         target.setHealth(target.getHealth() - spell.getDamage());
         caster.setHealth(caster.getHealth() + spell.getHealing());
-        return spell.getDamage();
     }
 
     public static boolean isAlive(Wizard wizard) {
         return wizard.getHealth() > 0;
+    }
+    public static int aiMove(){
+        Random random = new Random();
+        return random.nextInt(3);
+
     }
 }
